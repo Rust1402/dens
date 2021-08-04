6ee71cc22e79368287d5577b0b8ffed9b5a82872
Rust1402 <32862345+Rust1402@users.noreply.github.com>
2021-08-04 23:18:23 +0300

    Add files via upload

---
+++ js/scr.js
@@ -0,0 +1,17 @@
+
+
+
+// Меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+const headerMenu = document.querySelector('.header__menu');
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		headerMenu.classList.toggle('_active');
+	});
+}
+
+
+
+
